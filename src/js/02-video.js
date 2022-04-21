c02e657003b1e7a4c2d3b5dbe6247d7e3aba25f2
Serhii Demydenko <demydenko.serhii1994@gmail.com>
2022-04-21 17:27:31 +0300

    ewra

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
 
+function onPlay() {
+  console.log('played the video!');
+}
+player.on('play', onPlay);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(data) {
+  localStorage.setItem(LOCAL_STORAGE_KEY, data.seconds);
+}
+
+player
+  .setCurrentTime(localStorage.getItem(LOCAL_STORAGE_KEY))
+  .then(function (seconds = 0) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
 
+form.addEventListener('submit', onSubmitForm);
+form.addEventListener('input', throttle(onInputForm, 500));
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
+  form.reset();
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+}
+
+const saveDate = {};
+function onInputForm(e) {
+  const { name, value } = e.target;
+  saveDate[name] = value;
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(saveDate));
+}
+
+function onFillInput() {
+  const storObj = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  if (!storObj) return;
+  Object.keys(storObj).forEach(el => {
+    form.elements[el].value = storObj[el];
+  });
+}
+onFillInput();
